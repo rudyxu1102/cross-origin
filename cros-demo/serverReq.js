fda3a0c89955125179aabe93eb257bbf78245c47
FatDong1 <xuhaodong66@foxmail.com>
2017-06-13 11:30:13 +0800

    添加注释

--- cros-demo/serverReq.js
+++ cros-demo/serverReq.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
+var express = require('express'); // 引用express模块
+var app = express();  // 创建一个简单的服务器
 
 var requestPort = 3000;  // 请求页面跑在3000端口
 
 app.use(express.static(__dirname)); //3000端口的静态文件，即index.html
 app.listen(requestPort, function () {
-    console.log('Requester is listening on port '+ requestPort);
+    console.log('Requester is listening on port '+ requestPort);  // 在dos窗口会执行这个回调函数
 });
