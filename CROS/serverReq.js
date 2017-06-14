55503a35447e1a6afd42e00e2fd2839e2e3b6314
FatDong1 <xuhaodong66@foxmail.com>
2017-06-14 17:21:53 +0800

    CROS跨域

--- CROS/serverReq.js
+++ CROS/serverReq.js
@@ -5,7 +5,6 @@ var requestPort = 3000;  // 请求页面跑在3000端口
 
 app.use(express.static(__dirname)); //3000端口的静态文件，即index.html
 
-//__dirname为
 
 app.listen(requestPort, function () {
     console.log('Requester is listening on port '+ requestPort);  // 在dos窗口会执行这个回调函数
