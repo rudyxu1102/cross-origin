497f1aa58aa99bea6cd9fce1c9571b23c05165d2
FatDong1 <xuhaodong66@foxmail.com>
2017-06-14 16:07:36 +0800

    不使用插件设置代理跨域

--- proxy-demo/demo2/serverReq.js
+++ proxy-demo/demo2/serverReq.js
@@ -1,10 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
+var express = require('express');
+var http= require('http');
 
-</body>
-</html>
+var portNumber = 3008;
+var app = express();
+
+app.use(express.static(__dirname)); //即index.html
+
+app.get('/proxy', function(request, response){
+    var url = request.query.url    // http://localhost:3009/
+
+    console.log(url)
+
+    // 向url发出请求
+    http.get(url, function(responseFromOtherDomain) {
+        // data事件会在数据接收过程中，每收到一段数据就触发一次，接收到的数据被传入回调函数。
+        responseFromOtherDomain.on("data", function(data) {
+            response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            response.end(data);
+        });
+    });
+});
+
+app.listen(portNumber, function () {
+    console.log('Requester with proxy is listening on port '+ portNumber);
+});
--- proxy-demo/demo2/serverRes.js
+++ proxy-demo/demo2/serverRes.js
@@ -1,3 +1,12 @@
-/**
- * Created by ÐíºÆ¶« on 2017/6/14.
- */
+var express = require('express');
+
+var portNumber = 3009;
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("hello world from 3009");
+})
+
+app.listen(portNumber, function () {
+    console.log("Responding server listening on port "+ portNumber);
+});
