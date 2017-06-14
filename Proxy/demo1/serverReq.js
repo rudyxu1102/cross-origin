b12a73ad2d561332ab4c3c18b46d234ace9821c4
FatDong1 <xuhaodong66@foxmail.com>
2017-06-14 23:03:50 +0800

    使用http-proxy-middleware插件设置代理

--- Proxy/demo1/serverReq.js
+++ Proxy/demo1/serverReq.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 
-var requestPort = 3006;  // 请求页面跑在3006端口
+var requestPort = 3000;  // 请求页面跑在3006端口
 var app = express();
 
 app.use(express.static(__dirname));
 
-app.use('/api', proxy({target: 'http://localhost:3007/', changeOrigin: true}));
+app.use('/api', proxy({target: 'http://localhost:3001/', changeOrigin: true}));
 // changeOrigin设置为true，本地会虚拟一个服务端接收你的请求并代你发送该请求
-// http://localhost:3006/api   -->   http://localhost:3007/api
+// http://localhost:3000/api   -->   http://localhost:3001/api
 
 app.listen(requestPort, function () {
     console.log('Requester is listening on port '+ requestPort);
--- Proxy/demo1/serverRes.js
+++ Proxy/demo1/serverRes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var responsePort = 3007;  // 请求页面跑在3007端口
+var responsePort = 3001;  // 请求页面跑在3001端口
 
 app.get('/api', (req, res) => {
     res.send("Hello world from Proxy. ?")
