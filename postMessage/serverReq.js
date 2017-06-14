b024bdf1b6900e1b18836ecf6833e6de776f94d9
FatDong1 <xuhaodong66@foxmail.com>
2017-06-14 23:05:36 +0800

    HTML5新特性postMessage跨域

--- postMessage/serverReq.js
+++ postMessage/serverReq.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-var requestPort = 3004;  // 请求页面跑在3004端口
+var requestPort = 3000;
 
-app.use(express.static(__dirname + '/staticReq')); //3004端口的静态文件，即index.html
+app.use(express.static(__dirname + '/staticReq')); //3000端口的静态文件，即index.html
 
 app.listen(requestPort, function () {
     console.log('Requester is listening on port '+ requestPort);
--- postMessage/serverRes.js
+++ postMessage/serverRes.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-var responsePort = 3005;  // 请求页面跑在3004端口
+var responsePort = 3001;  // 请求页面跑在3001端口
 
-app.use(express.static(__dirname + '/staticRes')); //3005端口的静态文件，即index.html
+app.use(express.static(__dirname + '/staticRes')); //3001端口的静态文件，即index.html
 
 app.listen(responsePort, function () {
     console.log('Responser is listening on port '+ responsePort);
