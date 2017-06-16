8b170d54771c787f5a0d4dc112ee3edb9503d0f5
FatDong1 <xuhaodong66@foxmail.com>
2017-06-16 21:25:25 +0800

    CROS跨域

--- 1-CROS/serverReq.js
+++ 1-CROS/serverReq.js
@@ -5,10 +5,10 @@ var requestPort = 3000;  // 请求页面跑在3000端口
 
 app.use(express.static(__dirname + '/static')); //3000端口的静态文件，即index.html
 
-// __dirname始终指向当前js代码文件(serverReq)的目录, 在我的本地目录是D:\cross-domain\CROS
+// __dirname始终指向当前js代码文件(serverReq.js)所在的目录, 在我的本地目录是D:\cross-domain\CROS
 // console.log(__dirname) 在dos窗口可以看到
-// 上面express.static的意思项目的静态文件目录在 D:\cross-domain\CROS\static
-// 可以在static里面加入b.html, 然后在http://localhost:3000/b.html可以显示
+// 上面“express.static(__dirname + '/static')”的意思项目的静态文件目录在 D:\cross-domain\CROS\static
+// 可以在static里面随便加入b.html, 然后在http://localhost:3000/b.html可以显示
 
 app.listen(requestPort, function () {
     console.log('Requester is listening on port '+ requestPort);  // 在dos窗口会执行这个回调函数
--- 1-CROS/serverRes.js
+++ 1-CROS/serverRes.js
@@ -5,8 +5,8 @@ var responsePort = 3001;  // 响应请求的页面跑在3001端口
 
 // 可以改变‘/’为其他的路径，改完之后serverReq.js里面的请求路径也需要改变
 app.get('/', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000'); // 设置允许跨域的origin，允许3000端口访问3001端口
-    res.send("Hello world from CROS.😡")   // 空格部分为表情，可能在编辑器不会显示
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000'); // 设置允许跨域的origin，允许3000端口访问本端口（3001）
+    res.send("Hello world from CROS.😡");   // 空格部分为表情，可能在编辑器不会显示
 });
 
 app.listen(responsePort, function () {
